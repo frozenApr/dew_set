b406e0ed855e6cf572a2d86fe9c36bfd4c0dd9f5
sunbei <sunbei@sonkwo.com.cn>
2017-10-20 17:33:34 +0800

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,23 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(phone_number: params[:user][:phone_number])
+    user = User.ransack(phone_number_or_nick_name_or_email_eq:
+                        user_params[:phone_number_or_nick_name_or_email_eq]).result
     if user && user.authenticate(params[:user][:password])
       login_by user
       messenger notice: '登陆成功！'
     else
-      render :no_content
+      messenger notice: '账号或密码错误！'
     end
   end
 
   def destroy
     logout
     redirect_to :root
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:phone_number_or_nick_name_or_email_eq)
   end
 end
